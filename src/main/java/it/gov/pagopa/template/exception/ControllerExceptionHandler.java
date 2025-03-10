ed2069da063bafaef9bcf66119b937495c0ee8e3
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-03-10 15:39:03 +0100

    fix: PrintStackTrace if 5xx (#21)

--- src/main/java/it/gov/pagopa/template/exception/ControllerExceptionHandler.java
+++ src/main/java/it/gov/pagopa/template/exception/ControllerExceptionHandler.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ValidationException;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.event.Level;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -38,7 +39,7 @@ public class ControllerExceptionHandler {
     ErrorDTO.CodeEnum errorCode = ErrorDTO.CodeEnum.GENERIC_ERROR;
     if (ex instanceof ErrorResponse errorResponse) {
       httpStatus = errorResponse.getStatusCode();
-      if(httpStatus.isSameCodeAs(HttpStatus.NOT_FOUND)) {
+      if (httpStatus.isSameCodeAs(HttpStatus.NOT_FOUND)) {
         errorCode = ErrorDTO.CodeEnum.NOT_FOUND;
       } else if (httpStatus.is4xxClientError()) {
         errorCode = ErrorDTO.CodeEnum.BAD_REQUEST;
@@ -63,19 +64,24 @@ public class ControllerExceptionHandler {
   }
 
   private static void logException(Exception ex, HttpServletRequest request, HttpStatusCode httpStatus) {
-    log.info("A {} occurred handling request {}: HttpStatus {} - {}",
-      ex.getClass(),
-      getRequestDetails(request),
-      httpStatus.value(),
-      ex.getMessage());
-    if(log.isDebugEnabled() && ex.getCause()!=null){
+    boolean printStackTrace = httpStatus.is5xxServerError();
+    Level logLevel = printStackTrace ? Level.ERROR : Level.INFO;
+    log.makeLoggingEventBuilder(logLevel)
+      .log("A {} occurred handling request {}: HttpStatus {} - {}",
+        ex.getClass(),
+        getRequestDetails(request),
+        httpStatus.value(),
+        ex.getMessage(),
+        printStackTrace ? ex : null
+      );
+    if (!printStackTrace && log.isDebugEnabled() && ex.getCause() != null) {
       log.debug("CausedBy: ", ex.getCause());
     }
   }
 
   private static String buildReturnedMessage(Exception ex) {
     if (ex instanceof HttpMessageNotReadableException) {
-      if(ex.getCause() instanceof JsonMappingException jsonMappingException){
+      if (ex.getCause() instanceof JsonMappingException jsonMappingException) {
         return "Cannot parse body: " +
           jsonMappingException.getPath().stream()
             .map(JsonMappingException.Reference::getFieldName)
@@ -88,7 +94,7 @@ public class ControllerExceptionHandler {
         methodArgumentNotValidException.getBindingResult()
           .getAllErrors().stream()
           .map(e -> " " +
-            (e instanceof FieldError fieldError? fieldError.getField(): e.getObjectName()) +
+            (e instanceof FieldError fieldError ? fieldError.getField() : e.getObjectName()) +
             ": " + e.getDefaultMessage())
           .sorted()
           .collect(Collectors.joining(";"));
