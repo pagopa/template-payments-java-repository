cf3b649c69c836cba1310142cafca9acac877921
antonio.torre <antonio.torre@pagopa.it>
2025-04-11 18:37:02 +0200

    fix: Accepting LocalDateTime instead of OffsetDateTime in incoming JSON

--- src/main/java/it/gov/pagopa/template/utils/Calculator.java
+++
@@ -1,8 +0,0 @@
-package it.gov.pagopa.template.utils;
-
-public class Calculator {
-
-  public int multiply(int a, int b) {
-    return a * b;
-  }
-}
---
+++ src/main/java/it/gov/pagopa/template/utils/Constants.java
@@ -0,0 +1,12 @@
+package it.gov.pagopa.template.utils;
+
+import java.time.ZoneId;
+
+public class Constants {
+
+  private Constants(){}
+
+  public static final ZoneId ZONEID = ZoneId.of("Europe/Rome");
+
+}
+
