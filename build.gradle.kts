e371ab2d1e732535c95f1090effde3519f1143af
antonio.torre <antonio.torre@pagopa.it>
2025-05-14 17:48:27 +0200

    fix: CVE scan + libraries updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
   java
-  id("org.springframework.boot") version "3.4.3"
+  id("org.springframework.boot") version "3.4.5"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
-  id("org.sonarqube") version "6.0.1.5171"
-  id("com.github.ben-manes.versions") version "0.51.0"
-  id("org.openapi.generator") version "7.10.0"
+  id("org.sonarqube") version "6.1.0.5360"
+  id("com.github.ben-manes.versions") version "0.52.0"
+  id("org.openapi.generator") version "7.13.0"
   id("com.gorylenko.gradle-git-properties") version "2.5.0"
 }
 
@@ -29,10 +29,10 @@ repositories {
   mavenCentral()
 }
 
-val springDocOpenApiVersion = "2.8.5"
+val springDocOpenApiVersion = "2.8.6"
 val openApiToolsVersion = "0.2.6"
-val micrometerVersion = "1.4.3"
-val httpClientVersion = "5.4.2"
+val micrometerVersion = "1.4.6"
+val httpClientVersion = "5.4.4"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter")
