d372d99ca2fc1f8b8fcc30d696c920b4d2f7b7fe
antonio.torre <antonio.torre@pagopa.it>
2025-06-23 12:56:28 +0200

    fix: P4ADEV-3193 fix cve

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
   java
-  id("org.springframework.boot") version "3.5.0"
+  id("org.springframework.boot") version "3.5.3"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
-  id("org.sonarqube") version "6.1.0.5360"
+  id("org.sonarqube") version "6.2.0.5505"
   id("com.github.ben-manes.versions") version "0.52.0"
   id("org.openapi.generator") version "7.13.0"
   id("com.gorylenko.gradle-git-properties") version "2.5.0"
