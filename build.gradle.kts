00a016947fbc860216bc5f659ba5afca2b2e5e92
antonio.torre <antonio.torre@pagopa.it>
2025-09-26 10:40:39 +0200

    fix: Updating Spring version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   java
-  id("org.springframework.boot") version "3.5.3"
+  id("org.springframework.boot") version "3.5.6"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
   id("org.sonarqube") version "6.3.1.5724"
