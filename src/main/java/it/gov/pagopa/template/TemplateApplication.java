cf39d4cb1836cf7396f10feb5c5037492fdf6378
antonio.torre <antonio.torre@pagopa.it>
2025-12-19 16:22:41 +0100

    fix: Setting default TimeZone on main

--- src/main/java/it/gov/pagopa/template/TemplateApplication.java
+++ src/main/java/it/gov/pagopa/template/TemplateApplication.java
@@ -1,13 +1,17 @@
 package it.gov.pagopa.template;
 
+import it.gov.pagopa.template.utils.Constants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.webmvc.autoconfigure.error.ErrorMvcAutoConfiguration;
+
+import java.util.TimeZone;
 
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class TemplateApplication {
 
 	public static void main(String[] args) {
+    TimeZone.setDefault(Constants.DEFAULT_TIMEZONE);
 		SpringApplication.run(TemplateApplication.class, args);
 	}
 
--- src/main/java/it/gov/pagopa/template/utils/Constants.java
+++ src/main/java/it/gov/pagopa/template/utils/Constants.java
@@ -1,12 +1,14 @@
 package it.gov.pagopa.template.utils;
 
 import java.time.ZoneId;
+import java.util.TimeZone;
 
 public class Constants {
 
   private Constants(){}
 
   public static final ZoneId ZONEID = ZoneId.of("Europe/Rome");
+  public static final TimeZone DEFAULT_TIMEZONE = TimeZone.getTimeZone(ZONEID);
 
 }
 
