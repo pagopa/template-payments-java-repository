fde8da291a412c40074a5d4ae3c3b9c8544558e2
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2026-06-22 09:26:18 +0200

    fix: Fix CVE (#31)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,14 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
   java
-  id("org.springframework.boot") version "4.0.6"
+  id("org.springframework.boot") version "4.0.7"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
-  id("org.sonarqube") version "7.2.3.7755"
+  id("org.sonarqube") version "7.3.1.8318"
   id("com.github.ben-manes.versions") version "0.54.0"
-  id("org.openapi.generator") version "7.21.0"
-  id("com.gorylenko.gradle-git-properties") version "2.5.7"
-  id("com.github.jk1.dependency-license-report") version "3.1.2"
+  id("org.openapi.generator") version "7.23.0"
+  id("com.gorylenko.gradle-git-properties") version "4.0.1"
+  id("com.github.jk1.dependency-license-report") version "3.1.4"
 }
 
 group = "it.gov.pagopa.payhub"
@@ -49,11 +49,12 @@ repositories {
 }
 
 val springDocOpenApiVersion = "3.0.3"
-val janinoVersion = "3.1.12"
 val openApiToolsVersion = "0.2.10"
-val micrometerVersion = "1.6.5"
+val micrometerVersion = "1.7.0"
 val httpClientVersion = "5.6.1"
 val httpCoreVersion = "5.4.2"
+val kafkaAppender = "0.2.0-RC2"
+val lz4JavaVersion = "1.11.0"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
@@ -63,12 +64,15 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocOpenApiVersion")
-  implementation("org.codehaus.janino:janino:$janinoVersion")
   implementation("io.micrometer:micrometer-tracing-bridge-otel:$micrometerVersion")
   implementation("io.micrometer:micrometer-registry-prometheus")
   implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
   implementation("org.apache.httpcomponents.client5:httpclient5:$httpClientVersion")
   implementation("org.apache.httpcomponents.core5:httpcore5:$httpCoreVersion")
+  implementation("com.github.danielwegener:logback-kafka-appender:$kafkaAppender") {
+    exclude(group = "org.lz4", module = "lz4-java")
+  }
+  implementation("at.yawk.lz4:lz4-java:$lz4JavaVersion")
 
   compileOnly("org.projectlombok:lombok")
   annotationProcessor("org.projectlombok:lombok")
@@ -122,6 +126,7 @@ tasks {
       expand(projectInfo)
     }
   }
+  processResources.dependsOn("dependenciesBuild")
 }
 
 tasks.compileJava {
@@ -158,7 +163,8 @@ openApiGenerate {
     mapOf(
       "dateLibrary" to "java8",
       "requestMappingMode" to "api_interface",
-      "useSpringBoot3" to "true",
+      "useSpringBoot4" to "true",
+      "useJackson3" to "true",
       "interfaceOnly" to "true",
       "useTags" to "true",
       "useBeanValidation" to "true",
